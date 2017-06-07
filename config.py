ecdd60b1f404027fa22daa7596fc07838e4f1c85
Allan Swanepoel <support@linaccess.za.net>
2017-06-07 11:12:12 +0200

    Adding setup for docs

---
+++ config.py
@@ -0,0 +1,111 @@
+import os
+import sys
+from raygun4py.middleware import flask as flask_raygun
+
+PYTHON_VERSION = sys.version_info[0]
+if PYTHON_VERSION == 3:
+    import urllib.parse
+else:
+    import urlparse
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+if os.path.exists('config.env'):
+    print('Importing environment from .env file')
+    for line in open('config.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
+
+class Config:
+    APP_NAME = 'Flask-Base'
+    if os.environ.get('SECRET_KEY'):
+        SECRET_KEY = os.environ.get('SECRET_KEY')
+    else:
+        SECRET_KEY = 'SECRET_KEY_ENV_VAR_NOT_SET'
+        print('SECRET KEY ENV VAR NOT SET! SHOULD NOT SEE IN PRODUCTION')
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD') or 'password'
+    ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL') or 'admin@example.com'
+
+    MAIL_NAME = 'Cash Connect Capital'
+    MAIL_ADDRESS = 'no-reply@cashconnect.co.za'
+    MAIL_USER = MAIL_NAME + '<' + MAIL_ADDRESS + '>'
+    EMAIL_SUBJECT_PREFIX = '[{}]'.format(APP_NAME)
+    EMAIL_SENDER = '{app_name} Admin <{email}>'.format(app_name=APP_NAME, email=MAIL_USER)
+
+    REDIS_URL = os.getenv('REDISTOGO_URL') or 'http://localhost:6379'
+
+    RAYGUN_APIKEY = os.environ.get('RAYGUN_APIKEY')
+
+    if PYTHON_VERSION == 3:
+        urllib.parse.uses_netloc.append('redis')
+        url = urllib.parse.urlparse(REDIS_URL)
+    else:
+        urlparse.uses_netloc.append('redis')
+        url = urlparse.urlparse(REDIS_URL)
+
+    RQ_DEFAULT_HOST = url.hostname
+    RQ_DEFAULT_PORT = url.port
+    RQ_DEFAULT_PASSWORD = url.password
+    RQ_DEFAULT_DB = 0
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    ASSETS_DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    print('THIS APP IS IN DEBUG MODE. YOU SHOULD NOT SEE THIS IN PRODUCTION.')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    WTF_CSRF_ENABLED = False
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SSL_DISABLE = (os.environ.get('SSL_DISABLE') or 'True') == 'True'
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+        assert os.environ.get('SECRET_KEY'), 'SECRET_KEY IS NOT SET!'
+
+        flask_raygun.Provider(app, app.config['RAYGUN_APIKEY']).attach()
+
+
+class HerokuConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+        from werkzeug.contrib.fixers import ProxyFix
+        app.wsgi_app = ProxyFix(app.wsgi_app)
+
+
+class UnixConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+        import logging
+        from logging.handlers import SysLogHandler
+        syslog_handler = SysLogHandler()
+        syslog_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(syslog_handler)
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig,
+    'heroku': HerokuConfig,
+    'unix': UnixConfig
+}
--- docs/source/setup/index.rst
+++ docs/source/setup/index.rst
@@ -1,4 +1,120 @@
 .. _setup:
 
-Setup
-=====
+Setting up
+==========
+
+Clone the repo
+--------------
+
+::
+
+    $ git clone https://github.com/infrascloudy/flask-base.git
+    $ cd flask-base
+
+Initialize a virtualenv
+-----------------------
+
+::
+
+    $ pip install virtualenv
+    $ virtualenv env
+    $ source env/bin/activate
+
+(If you’re on a mac) Make sure xcode tools are installed
+--------------------------------------------------------
+
+::
+
+    $ xcode-select --install
+
+Add Environment Variables
+-------------------------
+
+| Create a file called ``.env`` that contains environment variables in
+  the following syntax: ``ENVIRONMENT_VARIABLE=value``. For example,
+| the mailing environment variables can be set as the following
+
+::
+
+    MAIL_NAME = 'My Visible Name'
+    MAIL_ADDRESS = 'no-reply@example.com'
+    SECRET_KEY=SuperRandomStringToBeUsedForEncryption
+
+**Note: do not include the ``.env`` file in any commits. This should
+remain private.**
+
+Install the dependencies
+------------------------
+
+::
+
+    $ pip install -r requirements/common.txt
+    $ pip install -r requirements/dev.txt
+
+Other dependencies for running locally
+--------------------------------------
+
+You need to install `Foreman`_ and `Redis`_. Chances are, these commands
+will work:
+
+::
+
+    $ gem install foreman
+
+Mac (using `homebrew`_):
+
+::
+
+    $ brew install redis
+
+Linux:
+
+::
+
+    $ sudo apt-get install redis-server
+
+Create the database
+-------------------
+
+::
+
+    $ python manage.py recreate_db
+
+Other setup (e.g. creating roles in database)
+---------------------------------------------
+
+::
+
+    $ python manage.py setup_dev
+
+Note that this will create an admin user with email and password
+specified by the ``ADMIN_EMAIL`` and ``ADMIN_PASSWORD`` config
+variables. If not specified, they are both
+``flask-base-admin@example.com`` and ``password`` respectively.
+
+[Optional] Add fake data to the database
+----------------------------------------
+
+::
+
+    $ python manage.py add_fake_data
+
+[Optional. Only valid on ``gulp-static-watcher`` branch] Use gulp to live compile your files
+--------------------------------------------------------------------------------------------
+
+-  Install the Live Reload browser plugin from `here`_
+-  Run ``npm install``
+-  Run ``gulp``
+
+Running the app
+---------------
+
+::
+
+    $ source env/bin/activate
+    $ foreman start -f Local
+
+.. _Foreman: https://ddollar.github.io/foreman/
+.. _Redis: http://redis.io/
+.. _homebrew: http://brew.sh/
+.. _here: http://livereload.com/
